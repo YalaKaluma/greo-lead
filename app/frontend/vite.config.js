a06f7c1a1f952f11798e202dd3999bf4efaf7324
YalaKaluma81 <yala.kaluma@simon-kucher.com>
2025-12-21 14:26:54 -0500

    FE/BE connection

--- app/frontend/vite.config.js
+++ app/frontend/vite.config.js
@@ -3,7 +3,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: './',  // <== This is the fix
   build: {
-    outDir: 'dist',
+    outDir: '../static',
+    emptyOutDir: true
   },
+  server: {
+    port: 5173
+  }
 })
