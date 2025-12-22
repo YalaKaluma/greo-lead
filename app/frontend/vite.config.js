9864bbec0c26de1c72fa09f2a20c7fae32bdce16
YalaKaluma81 <yala.kaluma@simon-kucher.com>
2025-12-21 19:34:02 -0500

    new code structure

--- app/frontend/vite.config.js
+++ app/frontend/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
@@ -25,6 +26,12 @@ export default defineConfig({
         target: 'http://localhost:8000',
         changeOrigin: true,
       }
+    }
+  },
+  
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
     }
   }
 })
