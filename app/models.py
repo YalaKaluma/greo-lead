3a7de307858a0c421f2b8c153bd7791d0c575fb6
YalaKaluma81 <yala.kaluma@simon-kucher.com>
2025-12-22 23:11:12 -0500

    authentication v3

--- app/models.py
+++ app/models.py
@@ -9,15 +9,6 @@ from sqlalchemy.dialects.postgresql import JSONB
 
 
 
-class User(Base):
-    __tablename__ = "users"
-
-    id = Column(Integer, primary_key=True, index=True)
-    phone_number = Column(String, unique=True, index=True)
-    name = Column(String)
-    created_at = Column(DateTime, default=datetime.utcnow)
-
-    entries = relationship("JournalEntry", back_populates="user")
 
 
 class JournalEntry(Base):
