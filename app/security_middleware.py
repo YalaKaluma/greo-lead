87d053e4b914a49ace3e59ed54ca471eadc1add2
YalaKaluma81 <yala.kaluma@simon-kucher.com>
2026-08-10 11:20:27 -0400

    Back end fix for android app

--- app/security_middleware.py
+++ app/security_middleware.py
@@ -77,6 +77,7 @@ class SecurityHeadersMiddleware(BaseHTTPMiddleware):
 def trusted_application_origins() -> set[str]:
     origins = {
         "http://localhost",
+        "https://localhost",
         "http://localhost:5173",
         "http://127.0.0.1:5173",
         "capacitor://localhost",
